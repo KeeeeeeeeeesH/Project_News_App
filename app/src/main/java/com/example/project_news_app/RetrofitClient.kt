6028ca76cf9af8642cc157b2251b9aef69935ebe
champqazwsxedc <champ.za2002@hotmail.com>
2024-05-24 22:08:25 +0700

    AddResetpasswordUI
    AddResetpasswordUI

--- app/src/main/java/com/example/project_news_app/RetrofitClient.kt
+++ app/src/main/java/com/example/project_news_app/RetrofitClient.kt
@@ -4,8 +4,8 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.3.58.145:5000/"
-    //private const val BASE_URL = "http://ip เครื่องตัวเอง:5000/"
+//  private const val BASE_URL = "http://10.3.58.145:5000/"
+    private const val BASE_URL = "http://192.168.0.177:5000/"
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
